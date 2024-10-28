54881179c28a4258cd26541f66d422463052ee5c
CesareMazzilli <cesaremazzilli98@gmail.com>
2024-10-28 14:07:42 +0100

    feat: corret

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -2,8 +2,8 @@
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
 
-const name = 'Cesare';
-console.log(name);
+const name = "Cesare";
+console.log(myName);
 
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
